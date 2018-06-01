40b93b5b18d32e8fb5f108d828d2c178e7f7f9d0
Ramesh Kumar <codeofnode@gmail.com>
2018-06-01 18:05:15 +0530

    arguments length validation in create tool bash script

--- devops/create-tool.sh
+++ devops/create-tool.sh
@@ -3,6 +3,11 @@ if [[ -n $(git status -s) ]]; then
   exit 1
 fi
 
+if [[ $# < 3 ]]; then
+  echo "Exactly 3 arguments required"
+  exit 1
+fi
+
 git checkout master
 node ./ct.js "$1" "$2" "$3"
 git checkout -b "$1"
