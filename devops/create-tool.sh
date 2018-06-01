e908ddc3f1e3f5ce4f87aebe1f8b069b325af411
Ramesh Kumar <codeofnode@gmail.com>
2018-06-01 17:58:39 +0530

    added files for previous commit

---
+++ devops/create-tool.sh
@@ -0,0 +1,9 @@
+if [[ -n $(git status -s) ]]; then
+  echo "There are uncommitted files. Aborting!!!"
+  exit 1
+fi
+
+git checkout master
+node ./ct.js "$1" "$2" "$3"
+git checkout -b "$1"
+git status
