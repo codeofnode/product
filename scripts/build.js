70a2048993b9f3002fdc2e37cbef185c8d3825ff
Ramesh Kumar <codeofnode@gmail.com>
2018-06-04 15:05:07 +0530

    index of submodules in dist

--- scripts/build.js
+++ scripts/build.js
@@ -6,9 +6,9 @@ delete pkg.scripts;
 delete pkg.nyc;
 delete pkg.babel;
 
-const secs = readdirSync('../src').filter(sec => (!sec.startsWith('.') && !sec.endsWith('.js')));
-const secIndexStr = readFileSync('../src/section.js').toString();
+const secs = readdirSync('./src').filter(sec => (!sec.startsWith('.') && !sec.endsWith('.js')));
+const secIndexStr = readFileSync('./src/section.js').toString();
 
-secs.forEach(sec => writeFileSync(`../src/${sec}/index.js`, secIndexStr));
+secs.forEach(sec => writeFileSync(`./dist/${sec}/index.js`, secIndexStr));
 
 writeFileSync('dist/package.json', JSON.stringify(pkg, null, 2));
