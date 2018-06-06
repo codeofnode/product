436f5da6eb833091853b1aa66653126a5ca5e282
Ramesh Kumar <codeofnode@gmail.com>
2018-06-06 14:35:26 +0530

    Revert "no app import"
    This reverts commit c2cb36f3686fb309ec3d37e57f32055b9443b652.

---
+++ src/appImport.js
@@ -0,0 +1,37 @@
+import pkg from './package.json';
+
+/**
+ * @module appImport
+ */
+
+/**
+  * The Import manager class
+  * @class
+  */
+
+class Manager {
+  /**
+   * Create an instance of Import Manager class
+   * @param {String} modulePrefix - the prefix for custom modules
+   */
+  constructor(modulePrefix) {
+    this.modulePrefix = modulePrefix;
+  }
+
+  /**
+   * initialize or loads a module
+   * @param {String} mod - name of the module
+   * @param {...*} [opts] - options to initialize the module with
+   * @return {Object} the instance the module
+   */
+  load(mod, ...opts) {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    let ToRet = require(`${this.modulePrefix}${mod}`);
+    if (opts.length) ToRet = new ToRet(...opts);
+    return ToRet;
+  }
+}
+
+const manager = (new Manager((pkg.config && pkg.config.modulePrefix) || ''));
+global.appImport = manager.load.bind(manager);
+export default Manager;
